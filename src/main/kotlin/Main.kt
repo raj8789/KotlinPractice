5447a1554366feb951274df436bb895fced60baa
raj8789 <raushanraj8789@gmail.com>
2025-01-28 13:17:26 +0530

    Kotlin Code Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+
+
+
+class Singleton private constructor(val message: String) {
+    companion object {
+        private var instance: Singleton? = null
+
+        fun getInstance(msg: String): Singleton {
+            if (instance == null) {
+                instance = Singleton(msg)
+            }
+            return instance!!
+        }
+    }
+}
+fun main() {
+    val obj = Singleton.getInstance("Hello, Singleton!")
+    println(obj.message) // Output: Hello, Singleton!
+}
